1ec4270171de371bbd0c48a5ddc4f3269c3879cf
Nikita <zhcoder@MacBook-Pro-Nikita-3.local>
2021-05-23 14:04:13 +1000

    start

--- src/index.js
+++ src/index.js
@@ -17,11 +17,49 @@ import {
 } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
 import bridge from '@vkontakte/vk-bridge';
+import "./css/main.css";
 bridge.send("VKWebAppInit", {});
-bridge.send("VKWebAppFlashSetLevel", {"level": 1});
+
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Привет, мир!'
+);
 
 const Example = () => {
   const { viewWidth } = useAdaptivity();
+
+  let index = 0;
+
+  function check(number_index) {
+    let elements = document.getElementsByName(index.toString());
+    if (elements.length > 0) {
+      if (elements[0].classList.contains('active')) {
+        bridge.send("VKWebAppFlashSetLevel", {"level": 1});
+      } else {
+        bridge.send("VKWebAppFlashSetLevel", {"level": 0});
+      }
+    }
+    index += 1;
+  }
+
+  function timer() {
+    if (index == 8) {
+      index = 0;
+    }
+
+    setTimeout(() => {
+        timer();
+        check();
+    }, 1000);
+  }
+
+  timer();
+
+  function handleClick(e) {
+    e.preventDefault();
+    e.target.classList.toggle('active');
+  }
 
   return (
     <AppRoot>
@@ -31,8 +69,16 @@ const Example = () => {
             <Panel id="main">
               <PanelHeader>VKUI</PanelHeader>
               <Group header={<Header mode="secondary">Items</Header>}>
-                <SimpleCell>Hello</SimpleCell>
-                <SimpleCell>World</SimpleCell>
+                  <div class="wrapper-cards"> 
+                      <div name="0" class="card" onClick={handleClick}></div>
+                      <div name="1" class="card" onClick={handleClick}></div>
+                      <div name="2" class="card" onClick={handleClick}></div>
+                      <div name="3" class="card" onClick={handleClick}></div>
+                      <div name="4" class="card" onClick={handleClick}></div>
+                      <div name="5" class="card" onClick={handleClick}></div>
+                      <div name="6" class="card" onClick={handleClick}></div>
+                      <div name="7" class="card" onClick={handleClick}></div>
+                  </div>
               </Group>
             </Panel>
           </View>
